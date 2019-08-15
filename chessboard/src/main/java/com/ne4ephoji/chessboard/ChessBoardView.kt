3cfa65475cd0efb15902968fcd06ee8728e90f81
ne4ephoji <ne4ephoji@gmail.com>
2019-08-15 15:12:40 +0300

    added ChessBoardView class

---
+++ chessboard/src/main/java/com/ne4ephoji/chessboard/ChessBoardView.kt
@@ -0,0 +1,12 @@
+package com.ne4ephoji.chessboard
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ChessBoardView @JvmOverloads constructor (
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : View(context, attrs, defStyleAttr, defStyleRes)
